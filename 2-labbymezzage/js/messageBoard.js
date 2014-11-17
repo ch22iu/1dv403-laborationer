b60c5f8c53ce11234aa2ed480b893ce70365619f
Christopher <ch22iu@student.lnu.se>
2014-11-17 01:59:51 +0100

    Adding attributes

--- 2-labbymezzage/js/messageBoard.js
+++ 2-labbymezzage/js/messageBoard.js
@@ -1,17 +1,19 @@
 "use strict";
 
 window.onload = function() {
-	var messageBoard = document.querySelector(".messageBoard");
+	var mBoard = document.querySelector(".messageBoard");
 	var check = 0;
 	
-	messageBoard.onclick = function() {
+	mBoard.onclick = function() {
 		var content = document.querySelector(".content");
 		var newMess = document.createElement("div");
 		newMess.setAttribute("id", "board" +check);
 		content.appendChild(newMess);
 		
-		var board = new MessageBoard("board" +check);
+		var board = new messageBoard("board" +check);
 		board.init();
+		
+		check += 1;
 		console.log(check);
 		return false;
 	};
@@ -32,14 +34,26 @@ function messageBoard(newBoard) {
 		var messageBox = document.createElement("div");
 		var header = document.createElement("header");
 		var h1 = document.createElement("h1");
-		
+		var button = document.createElement("input");
 		
 		// Lägger till attributen
+		header.setAttribute("class", "header");
+		messageBox.setAttribute("class", "message-box");
+		textarea.setAttribute("autofocus", "autofocus");
+		button.setAttribute("value", "Skicka");
+		button.setAttribute("class", "button send");
+		button.setAttribute("type", "submit");
 		
 		// Append
+		board.appendChild(header);
+		header.appendChild(h1);
+		h1.textContent = "LabbyMezzage v 1337";
 		board.appendChild(messageBox);
-		board.appendChild(spanMessageCount);
 		board.appendChild(textarea);
+		board.appendChild(button);
+		
+		button.addEventListener("click", false);
+		
 	}
 	
 }
